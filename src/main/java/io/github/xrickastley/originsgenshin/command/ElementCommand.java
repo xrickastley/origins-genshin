ae8c214230f2b2f571455fc74f37a94ba1274801
xrickastley <reodiqueridge10@gmail.com>
2025-07-22 21:31:48 +0800

    Added new "query" subcommand.

--- src/main/java/io/github/xrickastley/originsgenshin/command/ElementCommand.java
+++ src/main/java/io/github/xrickastley/originsgenshin/command/ElementCommand.java
@@ -4,6 +4,9 @@ import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 import java.util.List;
+import java.util.function.Function;
+
+import org.jetbrains.annotations.Nullable;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
@@ -15,9 +18,11 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import io.github.xrickastley.originsgenshin.component.ElementComponent;
 import io.github.xrickastley.originsgenshin.element.Element;
 import io.github.xrickastley.originsgenshin.element.ElementHolder;
+import io.github.xrickastley.originsgenshin.element.ElementalApplication;
 import io.github.xrickastley.originsgenshin.element.ElementalApplications;
 import io.github.xrickastley.originsgenshin.element.InternalCooldownContext;
 import io.github.xrickastley.originsgenshin.element.reaction.ElementalReaction;
+import io.github.xrickastley.originsgenshin.util.Array;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
@@ -28,6 +33,14 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
 public class ElementCommand {
+	private static final Function<ElementalApplication, String> TO_FRIENDLY_STRING = a -> {
+		final String element = a.getElement().toString().substring(0, 1).toUpperCase() + a.getElement().toString().substring(1).toLowerCase();
+
+		return a.isDuration()
+			? String.format("%.3fU %s (%.2fs left)", a.getCurrentGauge(), element, a.getRemainingTicks() / 20.0)
+			: String.format("%.3fU %s", a.getCurrentGauge(), element);
+	};
+
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
 		dispatcher.register(
 			CommandManager
@@ -81,6 +94,17 @@ public class ElementCommand {
 								.executes(ElementCommand::reduceElement)
 							)
 						)
+					)
+				)
+				.then(
+					literal("query")
+					.then(
+						argument("target", EntityArgumentType.entity())
+						.then(
+							argument("element", ElementArgumentType.element())
+							.executes(ElementCommand::queryElement)
+						)
+						.executes(ElementCommand::queryElements)
 					)
 				)
 		);
@@ -217,6 +241,61 @@ public class ElementCommand {
 			);
 
 		return 1;
+	}
+
+	private static int queryElements(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+		final Entity entity = EntityArgumentType.getEntity(context, "target");
+
+		if (!(entity instanceof final LivingEntity target)) {
+			context.getSource().sendError(Text.literal("The provided target must be a living entity!").formatted(Formatting.RED));
+
+			return 0;
+		}
+
+		final ElementComponent component = ElementComponent.KEY.get(target);
+		final Array<ElementalApplication> appliedElements = component.getAppliedElements();
+
+		if (appliedElements.isEmpty()) {
+			context.getSource().sendError(Text.literal("The provided target has no applied elements!").formatted(Formatting.RED));
+
+			return 0;
+		}
+
+		final String message = "Applied elements: " + appliedElements
+			.map(ElementCommand.TO_FRIENDLY_STRING)
+			.join(", ");
+
+		context
+			.getSource()
+			.sendFeedback(() -> Text.literal(message), false);
+
+		return 1;
+	}
+
+	private static int queryElement(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+		final Entity entity = EntityArgumentType.getEntity(context, "target");
+		final Element element = ElementArgumentType.getElement(context, "element");
+
+		if (!(entity instanceof final LivingEntity target)) {
+			context.getSource().sendError(Text.literal("The provided target must be a living entity!").formatted(Formatting.RED));
+
+			return 0;
+		}
+
+		final ElementComponent component = ElementComponent.KEY.get(target);
+		final @Nullable ElementalApplication application = component.getElementHolder(element).getElementalApplication();
+
+		if (application == null) {
+			context.getSource().sendError(Text.literal("The provided target doesn't have the \"" + element + "\" element applied!").formatted(Formatting.RED));
+
+			return 0;
+		}
+
+		context
+			.getSource()
+			.sendFeedback(() -> Text.literal("Element: " + ElementCommand.TO_FRIENDLY_STRING.apply(application)), false);
+
+		return application != null ? 1 : 0;
 	}
 
 	private static <T> T getOrDefault(CommandContext<ServerCommandSource> context, String name, Class<T> clazz, T fallback) {
